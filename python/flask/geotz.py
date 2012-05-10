85badd336ca001043c46e2dc3ed91425145c5ff0
Mark Allen <mrallen1@yahoo.com>
2012-05-10 17:49:29 -0500

    Add flask implementation

---
+++ python/flask/geotz.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, make_response
+from pytz import timezone
+from datetime import datetime
+import json
+import GeoIP
+
+DEBUG=True
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+gi = GeoIP.open("/usr/local/share/GeoIP/GeoLiteCity.dat", GeoIP.GEOIP_MEMORY_CACHE)
+
+@app.route('/', methods=['POST'])
+def lookup():
+    rv = dict()
+    for ip in request.json:
+        r = gi.record_by_addr(ip)
+        tz = timezone(r['time_zone'])
+        rv[ip] = { 
+            'country': r['country_name'],
+            'region': r['region'],
+            'city': r['city'],
+            'time_zone': r['time_zone'],
+            'utc_offset': int(tz.utcoffset(datetime.utcnow()).total_seconds())
+        }
+
+    response = make_response(json.dumps(rv))
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+if __name__ == '__main__':
+    app.run()
+
