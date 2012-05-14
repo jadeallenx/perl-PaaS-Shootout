ca89488e71e22747b0ad1196615824e166bb7e06
Mark Allen <mallen@alertlogic.com>
2012-05-14 10:48:59 -0500

    Make POST return JSON

--- erlang/webmachine/geotz/src/geotz_resource.erl
+++ erlang/webmachine/geotz/src/geotz_resource.erl
@@ -2,7 +2,7 @@
 %% @copyright 2012 Mark Allen.
 
 -module(geotz_resource).
--export([init/1, process_post/2, post_is_create/2, to_json/2, allowed_methods/2, content_types_provided/2]).
+-export([init/1, process_post/2, post_is_create/2, allowed_methods/2]).
 
 -include_lib("webmachine/include/webmachine.hrl").
 -include_lib("egeoip/include/egeoip.hrl").
@@ -12,37 +12,34 @@ init([]) -> {ok, []}.
 allowed_methods(ReqData, Context) ->
     {['POST'], ReqData, Context}.
 
-content_types_provided(ReqData, Context) ->
-    {[{"application/json", to_json}], ReqData, Context}.
-
 post_is_create(ReqData, Context) ->
     {false, ReqData, Context}.
 
 process_post(ReqData, Context) ->
     L0 = mochijson2:decode(wrq:req_body(ReqData)),
     Out = process_ips([ binary_to_list(Ip) || Ip <- L0 ]),
-    io:format("Out> ~p~n", [Out]),
-    to_json(ReqData, [{output, Out}] ++ Context),
-    {true, ReqData, Context}.
+    {true, set_content_type(wrq:set_resp_body(to_json(Out), ReqData), "application/json"), Context}.
 
-to_json(ReqData, Context) ->
-    io:format("Context> ~p~n", [Context]),
-    Json = mochijson2:encode({struct, proplists:get_value(output, Context)}),
-    io:format("Json> ~p~n", [Json]),
-    {Json, ReqData, Context}.
+to_json(Out) ->
+    Json = mochijson2:encode(Out),
+    list_to_binary(lists:flatten(Json)).
+
+set_content_type(ReqData, ContentType) ->
+    wrq:set_resp_headers([{"Content-Type", ContentType}], ReqData).
 
 process_ips(IpList) ->
-    io:format("IpList> ~p~n", [IpList]),
     F = fun (Ip, Acc) ->
         case egeoip:lookup(Ip) of
             {ok, R} ->
-                [{struct, [
-                    {<<"country">>, ensure_binary(egeoip:get(R, country_name))},
-                    {<<"region">>, ensure_binary(egeoip:get(R, region))},
-                    {<<"city">>, ensure_binary(egeoip:get(R, city))},
-                    %% this is an integer, so no need to make it a binary
-                    {<<"utc_offset">>, egeoip_tz:utc_offset(R)} ]
-                }] ++ Acc;
+                [ { ensure_binary(Ip), 
+                    [ {<<"country">>, geoip_get(R, country_name)},
+                      {<<"region">>, geoip_get(R, region)},
+                      {<<"city">>, geoip_get(R, city)},
+                      %% this is an integer, so no need to make it a binary
+                      {<<"utc_offset">>, egeoip_tz:utc_offset(R)} 
+                    ] 
+                  } 
+                ] ++ Acc;
             Error ->
                 erlang:error(Error)
         end
@@ -60,3 +57,5 @@ ensure_binary(Term) ->
             erlang:error(badtype)
     end.
     
+geoip_get(Record, Field) ->
+    ensure_binary(egeoip:get(Record, Field)).
